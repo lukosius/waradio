5d15bd181575943d9fc12a44230b2781cb4fc377
Gedas Lukošius <g3dut1s@gmail.com>
2016-03-19 11:50:31 +0200

    Add initial station edit window. #1

---
+++ WARadio/WARadio/EditWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WARadio
+{
+    /// <summary>
+    /// Interaction logic for EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : MetroWindow
+    {
+        public EditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
