9a2d09b321790d2b8af67e6edb75febecb30a555
Gedas Lukošius <g3dut1s@gmail.com>
2016-03-31 15:27:15 +0300

    Fixes #1

--- WARadio/WARadio/MainWindow.xaml.cs
+++ WARadio/WARadio/MainWindow.xaml.cs
@@ -24,5 +24,57 @@ namespace WARadio
             MetroWindow s = new FavoritesWindow();
             s.ShowDialog();
         }
+
+        private void ClickPlay(object sender, RoutedEventArgs e)
+        {
+            if (ButtonPlay.Tag.Equals("play"))
+            {
+                ButtonPlay.Content = FindResource("stop");
+                ButtonPlay.Tag = "stop";
+            }
+            else
+            {
+                ButtonPlay.Content = FindResource("play");
+                ButtonPlay.Tag = "play";
+            }
+        }
+
+        private void ClickButtonFavorites(object sender, RoutedEventArgs e)
+        {
+            if (ButtonFavorites.Tag.Equals("favorite"))
+            {
+                ButtonFavorites.Content = FindResource("unfavorite");
+                ButtonFavorites.Tag = "unfavorite";
+            }
+            else
+            {
+                ButtonFavorites.Content = FindResource("favorite");
+                ButtonFavorites.Tag = "favorite";
+            }
+        }
+
+        private void SliderVolumeChanged(object sender, RoutedEventArgs e)
+        {
+            if (SliderVolume.Value >= 100)
+            {
+                ButtonMute.Content = FindResource("volume_3");
+            }
+            else if (SliderVolume.Value >= 50)
+            {
+                ButtonMute.Content = FindResource("volume_2");
+            }
+            else if (SliderVolume.Value > 1)
+            {
+                ButtonMute.Content = FindResource("volume_1");
+            }
+            else if (SliderVolume.Value > 0)
+            {
+                ButtonMute.Content = FindResource("volume_0");
+            }
+            else
+            {
+                ButtonMute.Content = FindResource("volume_mute");
+            }
+        }
     }
 }
